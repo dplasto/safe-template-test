d49d7492a3b3d3f5b3c0c86e760de08d9222685b
dplasto <dplasto@gmail.com>
2021-02-16 23:06:25 +0000

    chart

--- src/Client/Index2.fs
+++ src/Client/Index2.fs
@@ -7,16 +7,25 @@ open SavingsTracker
 
 type State = {
     FinancialModel : FinancialModel
+    NewAccountName : string
+    NewAccountCash : int option
 }
 
 type Msg =
     | AddAccount
+    | UpdateNewAccountName of newAccountName : string
 
-let init() = { FinancialModel = FinancialModel.create "test" 0 }, Cmd.none
+let init() =
+    {
+        FinancialModel = FinancialModel.create "test" 0
+        NewAccountName = ""
+        NewAccountCash = None
+    }, Cmd.none
 
 let update (msg: Msg) (state: State) =
     match msg with
     | AddAccount -> state, Cmd.none
+    | UpdateNewAccountName newAccountName -> { state with NewAccountName = newAccountName }, Cmd.none
 
 let render (state: State) (dispatch: Msg -> unit) =
     Html.div [
@@ -25,11 +34,17 @@ let render (state: State) (dispatch: Msg -> unit) =
             prop.text "Add account"
         ]
 
-        Html.textf [
-            prop.onKeyUp (fun _ -> dispatch)
+        Bulma.input.text [
+            prop.placeholder "Account name"
+            prop.value state.NewAccountName
+            prop.onChange (UpdateNewAccountName  >> dispatch)
         ]
 
-        Feliz.React.
+        Bulma.input.number [
+            prop.placeholder "Account cash"
+            prop.value (Option.defaultValue 0 state.NewAccountCash)
+        ]
+
 
         Html.h1 state.FinancialModel.Description
 
